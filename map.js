4b4a990fb2113ae8dfe07268439bbf46d5004c08
daniel-drake <ddrakepgh@gmail.com>
2021-03-08 09:01:22 -0500

    refactored into better containers

--- map.js
+++ map.js
@@ -1,100 +1,7 @@
-console.log("starting");
-// map is made of 20pix x 20pix blocks
-// right now, each map is just a filled rect
-
-const TILE_WIDTH = 20;
-const TILE_HEIGHT = 20;
-
-var terrain = [ "grass", "trees", "dirt", "water" ];
-var topPixel = 0;
-var leftPixel = 0;
-var width = 400;
-var height = 600;
-var topRow = 0;
-var leftCol = 0;
-var mapWidth = width / TILE_WIDTH;
-var mapHeight = height / TILE_HEIGHT; 
-
-// this hold the terrain to draw
-var map = [];
-
-function calcMapOffset(col, row){
-    return row * mapWidth + col;
-}
-
-function Tile(type, col, row) {
-  this.type = type;
-  this.row = row;
-  this.col = col;
-  this.pixelCol = col * TILE_WIDTH;
-  this.pixelRow = row * TILE_HEIGHT;
-}
-
-Tile.prototype.render = function() {
-  switch(this.type) {
-  case "grass":
-    color = "LightGreen";
-    break;
-  case "trees":
-    color = "DarkOliveGreen";
-    break;
-  case "dirt":
-    color = "SandyBrown";
-    break;
-  case "water":
-    color = "RoyalBlue";
-    break;
-  default:
-    color = "Black";
-    break;
-  }
-
-  var pixelX = this.pixelCol - leftPixel;
-  var pixelY = this.pixelRow - topPixel;
-  context.fillStyle = color;
-  context.fillRect(pixelX, pixelY, TILE_WIDTH, TILE_HEIGHT);
-}
-
-function getRandomTerrain(){
-   return terrain[Math.floor(Math.random() * Math.floor(terrain.length))]; 
-}
-
-//populate terrain
-function populateMap(){
-    console.log("creating map");
-    var y;
-    var x;
-    for(y = 0; y < mapHeight; ++y){
-	for(x = 0; x < mapWidth; ++x){
-	    var type = getRandomTerrain();
-	    var offset = calcMapOffset(x, y);
-	    var tile = new Tile(type, y, x);
-	    map.push(tile);
-	}
-    }
-}
-
-function DrawMap(){
-    var topTile = 0;
-    var leftTile = 0;
-    var bottomTile = mapHeight; 
-    var rightTile =  mapWidth;
-
-    var y;
-    var x;
-    for(y = topTile; y < bottomTile; y++){
-	for(x = leftTile; x <rightTile; x++){
-	    var offset = calcMapOffset(x, y);
-	    map[offset].render();
-	}
-    }
-    
-}   
-
 var animate = window.requestAnimationFrame ||
-  window.webkitRequestAnimationFrame ||
-  window.mozRequestAnimationFrame ||
-  function(callback) { window.setTimeout(callback, 1000/60) };
+window.webkitRequestAnimationFrame ||
+window.mozRequestAnimationFrame ||
+function(callback) { window.setTimeout(callback, 1000/60) };
 
 var canvas = document.createElement('canvas');
 canvas.width = width;
@@ -102,24 +9,127 @@ canvas.height = height;
 var context = canvas.getContext('2d');
 
 window.onload = function() {
-  document.body.appendChild(canvas);
-  animate(step);
+    document.body.appendChild(canvas);
+    animate(step);
 };
 
-populateMap();
+console.log("starting");
+
+function Coord(x,y){
+    this.x = x;
+    thix.y = y;
+}
+
+function Tile(type) {
+    this.type = type;
+}
+
+Tile.prototype.render = function(topLeft, size) {
+    switch(this.type) {
+	case "grass":
+	    color = "LightGreen";
+	    break;
+	case "trees":
+	    color = "DarkOliveGreen";
+	    break;
+	case "dirt":
+	    color = "SandyBrown";
+	    break;
+	case "water":
+	    color = "RoyalBlue";
+	    break;
+	default:
+	    color = "Black";
+	    break;
+    }
+
+    context.fillStyle = color;
+    context.fillRect(topLeft.x, topLeft.y, size.x, size.y);
+}
+
+function Map(tileWidth, tileHeight, cols, rows){
+    this.tileWidth = tileWidth;
+    this.tileHeight = tileHeight;
+    this.cols = cols;
+    this.rows = rows;
+    this.terrain = [ "grass", "trees", "dirt", "water" ];
+    this.contents = [];
+}
+
+Map.prototype.calcOffset(x, y){
+    return y * this.cols + x;
+}
+
+Map.prototype.get(x, y){
+    var offset = this.calcOffset(x,y);
+    return contents[offset];
+}
+
+Map.prototype.set(tile, x, y){
+    var offset = this.calcOffset(x,y);
+    contents[offset] = tile; 
+}
+
+Map.prototype.getRandomTerrain(){
+    return this.terrain[Math.floor(Math.random() * Math.floor(this.terrain.length))]; 
+}
+
+Map.prototype.populate(){
+    var y;
+    var x;
+    for(y = 0; y < this.rows; ++y){
+	for(x = 0; x < this.cols; ++x){
+	    var type = this.getRandomTerrain();
+	    var tile = new Tile(type);
+	    this.contents.push(tile);
+	}
+    }
+}
+
+function ViewPort(topLeft, bottomRight){
+    this.topLeft = topLeft;
+    this.bottomRight = bottomRight;
+    this.updateOccurred = false;
+}
+
+ViewPort.prototype.render(map){
+    var topTile = Math.floor(this.topLeft.y / this.tileHeight);
+    var leftTile = Math.floor(this.topLeft.x / this.tideWidth);
+    var bottomTile = Math.floor(this.bottomRight.y / this.tileHeight);
+    var rightTile = Math.floor(this.bottomRight.x / this.tileWidth);
+    console.log("viewport is (", leftTile, ",", topTile, ") (", rightTile, ",", bottomTile, ")");
+
+    // only redraw the scrren if it was updated
+    if (this.updateOccurred){
+	var x,y;
+	for (y = topTile; y < bottomTile; ++y){
+	    for(x = leftTile, x < rightTile, ++x){
+		var tile = map.get(x, y);
+		var coord = new Coord(x*this.tileWidth, y * this.tileHeight);
+		var size = new Coord(this.tileWidth, this.tileHeight);
+		//tile.render(coord, size);
+	    }
+	}
+    }
+}
+
+var map = new Map(20, 20, 50, 50);
+map.populate();
+var initViewPortStart(0,0);
+var initviewPortEnd(400,600);
+var viewPort new ViewPort(initViewPortStart, initViewPortEnd);
 
 var step = function() {
-  update();
-  render();
-  animate(step);
+    update();
+    render();
+    animate(step);
 };
 
 var update = function() {
 };
 
 var render = function() {
-    DrawMap();
+    viewPort.render(map);
 };
-
 
 
