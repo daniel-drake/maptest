12dcd88dac2123e8ee99222c09f47985ed25228b
daniel-drake <ddrakepgh@gmail.com>
2021-03-11 10:31:27 -0500

    added comments and tank collision detection

--- tank/tank.js
+++ tank/tank.js
@@ -1,22 +1,39 @@
+//
+// this is a test framework for a simple game where two
+// tanks battle each other. There is only 1 shot allowed
+// at a time (per tank) and the shot can only go a certain
+// distance so the tanks must get relatively close together. 
 
+//this part is infrasturture framework
+// it sets the animate function that is supported
+// by the browser being used.
 var animate = window.requestAnimationFrame ||
 window.webkitRequestAnimationFrame ||
 window.mozRequestAnimationFrame ||
 function(callback) { window.setTimeout(callback, 1000/60) };
 
+// this is the width and height of the play field
 var canvasWidth = 800;
 var canvasHeight = 400;
 
+// create the canvas.  a canvas is what we are drawing on
 var canvas = document.createElement('canvas');
 canvas.width = canvasWidth;
 canvas.height = canvasHeight;
 var context = canvas.getContext('2d');
 
+// add the canvas to the window so
+// we can see what we are drawing
 window.onload = function() {
     document.body.appendChild(canvas);
     animate(step);
 };
 
+// this gets run every frame, it updates positions
+// draws the objects and checks for collisions.
+// each call is a one shot deal, so if we want
+// this function called again, we need to make
+// a call to animate each time
 var step = function() {
     update();
     render();
@@ -24,11 +41,15 @@ var step = function() {
     animate(step);
 };
 
+// this updates the tank and bullets
+// positions for each player
 var update = function() {
     player1.update();
     player2.update();
 };
 
+// this repaints the backgrouns and
+// draws the tanks and bullets foreach player
 var render = function() {
     context.fillStyle = "Black";
     context.fillRect(0, 0, canvas.width,canvas.height);
@@ -36,6 +57,7 @@ var render = function() {
     player2.render();
 };
 
+// keysDown keeps track of the keys currently pressed
 var keysDown = {};
 
 window.addEventListener("keydown", function(event) {
@@ -46,17 +68,23 @@ window.addEventListener("keyup", function(event) {
   delete keysDown[event.keyCode];
 });
 
+// this is an infrasturcture object
 function Circle(x, y, radius){
     this.x = x;
     this.y = y;
     this.radius = radius;
 };
 
+// this is an infrasturcture object
 function Coord(x,y){
     this.x = x;
     this.y = y;
 };
 
+// this is the tank object.  rotation
+// is in radians and is off by PI/2 because
+// of how the tank is drawn.  It is facing up
+// pi/2 instead of right which would be 0 PI 
 function Tank(width,height, x, y){
     this.width = width;
     this.height = height;
@@ -66,6 +94,8 @@ function Tank(width,height, x, y){
     this.yPos = y;
 };
 
+// support function to return the center
+// of a tank
 Tank.prototype.getCenter = function(){
     var xCenter = this.xPos + this.width / 2;
     var yCenter = this.yPos + this.height / 2;
@@ -73,12 +103,16 @@ Tank.prototype.getCenter = function(){
     return center;
 }
 
+// support function used to determine collisions
 Tank.prototype.getBoundingCircle = function(){
     var center = this.getCenter();
     var circle = new Circle(center.x, center.y, this.width / 2);
     return circle;
 }
 
+// this draws a tank.  The tank is drawn
+// facing up, but we set the rotation to
+// allow it to face different directions
 Tank.prototype.render = function(id){
 
     context.save();
@@ -135,7 +169,7 @@ Tank.prototype.render = function(id){
     // turrent
     context.beginPath();
     if (id == 1){
-	context.fillStyle = "Red";
+	context.fillStyle = "DarkKhaki";
     }
     else if (id == 2){
 	context.fillStyle = "Brown";
@@ -154,6 +188,8 @@ Tank.prototype.render = function(id){
     context.restore();
  };
 
+// update tells the tank how to move, this function
+// actually does the move
 Tank.prototype.move = function(rotation, speed){
 
     if (rotation != 0){
@@ -170,51 +206,46 @@ Tank.prototype.move = function(rotation, speed){
 	this.speed = speed;
 	// adjusting by Math.PI/2 is correcting direction for front
 	// of tank
+
+	var prevXPos = this.xPos;
+	var prevYpos =this.yPos;
+
+	// we are moving along an angle,  the cos is the x coord in
+	// the unit circle and the sin is the y coord in 
+	// the unit circle We multiply those number by
+	// the speed  to get the number of pixels to move on each axis
 	this.xPos += this.speed * Math.cos(this.rotation + Math.PI/2);
 	this.yPos += this.speed * Math.sin(this.rotation + Math.PI/2);
 
-	if (this.xPos > canvas.width){
-	    this.xPos = canvas.width;
+	// make sure we stay on the screen
+	if (this.xPos + this.width > canvas.width){
+	    this.xPos = canvas.width - this.width;
 	}
 
 	if (this.xPos < 0){
 	    this.xPos = 0;
 	}
 
-	if (this.yPos > canvas.height){
-	    this.yPos = canvas.height;
+	if ((this.yPos + this.height) > canvas.height){
+	    this.yPos = canvas.height - this.height;
 	}
 
 	if (this.yPos < 0){
 	    this.yPos = 0;
 	}
+
+	// don't move if we are going to be in a collision
+	if (didCollide(player1.tank, player2.tank)){
+	    this.xPos = prevXPos;
+	    this.yPos = prevYpos;
+	}
     }
 }
 
-Tank.prototype.update = function(){
-    var rotation = 0;
-    var speed = 0;
-    
-    for(var key in keysDown) {
-	var value = Number(key);
-	if(value == 37) { // left arrow
-	    rotation += -.1;
-	} else if (value == 39) { // right arrow
-	    rotation += .1;
-	}
-	else if (value == 38) { // up arrow
-	    speed = -3;
-	}
-	else if (value == 40) { // down arrow
-	    speed = 3;
-	}
-    }
-
-    if ((rotation != 0) || (speed != 0)){
-	this.move(rotation, speed);
-    }
-}
-
+// this is the bullet object
+// display is set to true when a shot is in progress
+// max distance is the max distance a shot can travel
+// angleRadians is the angle that the bullet is moving along
 function Bullet(){
     this.x = 0;
     this.y = 0;
@@ -227,6 +258,7 @@ function Bullet(){
     this.radius = 4;
 }
 
+// this sets the start point and the angle for a bullet
 Bullet.prototype.setVector = function(x, y, angleRadians){
     this.x = x;
     this.y = y;
@@ -235,28 +267,39 @@ Bullet.prototype.setVector = function(x, y, angleRadians){
     this.angleRadians = angleRadians;
 }
 
+// this sets the currDistance (which we don't really need
+// and the speed of the bullet
 Bullet.prototype.initDistance = function(){
     this.currDistance = 0;
     this.speed = -6;
 }
 
+// this function sets the current position of the bullet
+// PI/2 is used to correct for the tank facing up treated
+// as 0 instead of the tank facing right being treated as 0
 Bullet.prototype.move = function(){
+    // cos is the x coord, sin is the y coord 
     var distX = this.speed * Math.cos(this.angleRadians + Math.PI/2);
     var distY = this.speed * Math.sin(this.angleRadians + Math.PI/2);
     this.x += distX;
     this.y += distY;
 
+    // get variable to calculate distance
     var x1 = this.x;
     var x2 = this.initialX;
     var y1 = this.y;
     var y2 = this.initialY;
 
     var dist = Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2-y1), 2));
+
+    // if the distance traveled is over the limit,stop
+    // displaying the bullet
     this.currDistance = (Math.abs(dist));
     if (this.currDistance >= this.maxDistance){
 	this.display = false;
     }
 
+    // if the bullet goes off the screen stop displaying it
     if (this.x > canvas.width){
 	this.display = false;
     }
@@ -274,6 +317,7 @@ Bullet.prototype.move = function(){
     }
 }
 
+// draw the bullet
 Bullet.prototype.render = function(){
     if (this.display){
 	context.beginPath();
@@ -283,17 +327,21 @@ Bullet.prototype.render = function(){
     }
 }
 
+// used to detect collisions
 Bullet.prototype.getBoundingCircle = function(){
     var circle = new Circle(this.x, this.y, this.radius); 
     return circle;
 }
 
+// a player encapsulates a tank and a bullet
+// the controls are different per player
 function Player(id, x, y){
     this.tank = new Tank(50, 50, x, y);
     this.bullet = new Bullet();
     this.id = id;
 }
 
+// update the positions of the tank and bullet
 Player.prototype.update = function(){
 
     var rotation = 0;
@@ -335,6 +383,11 @@ Player.prototype.update = function(){
 	    speed = 3;
 	}
 	else if(value == shoot) {  
+	    // set the bullet to display
+	    // and set the initial position
+	    // which is the top of the gun muzzle.
+	    // we need to do a little math to figure
+	    // out where that is
 	    this.bullet.display = true;
 	    this.bullet.initDistance();    
 	    var center = this.tank.getCenter();
@@ -353,14 +406,18 @@ Player.prototype.update = function(){
     }
 }
 
+// draw the tank and bullet 
 Player.prototype.render = function(){
     this.tank.render(this.id);
     this.bullet.render();
 }
 
+// here are the two players
 var player1 = new Player(1, 20, 20);
 var player2 = new Player(2, 80, 80);
 
+// this function determines if two objects
+// collided
 function didCollide( obj1, obj2){
     var collision = false;
     var circle1 = obj1.getBoundingCircle();
@@ -376,6 +433,8 @@ function didCollide( obj1, obj2){
     return collision;
 }
 
+// this function will check if there were any
+// collisions
 function checkForCollisions(){
     // check if player1 bullet hit player2 tank
     if (player1.bullet.display){
@@ -384,6 +443,8 @@ function checkForCollisions(){
 	    console.log ("player1 hit player2");
 	}
     }
+
+    // check if player1 bullet hit player1 tank
     if (player2.bullet.display){
 	if (didCollide(player2.bullet, player1.tank)){
 	    player2.bullet.display = false;
